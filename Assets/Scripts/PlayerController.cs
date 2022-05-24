c5a730689188b53bac9c43d317e624c7ac81262c
AcePrimeOriginal <guzmanjonathan121204@gmail.com>
2022-05-24 11:58:19 -0400

    Commit to Main
    Day 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10;
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        float rightInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
     }
 }
