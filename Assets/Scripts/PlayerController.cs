eeea40b1f508023cf14c6eec8267b4b414d8d978
AcePrimeOriginal <guzmanjonathan121204@gmail.com>
2022-05-25 11:56:44 -0400

    Commit to main
    Day 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,12 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float turnSpeed = 10;
     public float forwardInput;
-    private Animator playerAnim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        playerAnim = GetComponent<Animator>();
+    
     }
 
     // Update is called once per frame
@@ -26,6 +26,5 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
-        playerAnim.SetTrigger("Jump_trig");
     }
 }
